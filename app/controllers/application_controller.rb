37642bd67b082950adc09f538055127751ab0939
umairrazam <omairrr.azam@gmail.com>
2021-06-15 21:08:53 +0500

    fix(fix the design of mobile nav bar, turbolink issue and currency bar design)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :load_static_footer_pages
+
+  def load_static_footer_pages
+    @static_footer_pages = Spree::Page.by_store(current_store).visible.footer_links
+  end    
 end
